0f71b765d693afccc414dd689332b7a4e9fe0d38
BjorRiz <mfarizalhaq1903@gmail.com>
2023-01-30 18:31:21 +0700

    PHP

--- Perpustakaan/ListBuku.php
+++ Perpustakaan/ListBuku.php
@@ -1,6 +1,4 @@
-<?php
-
-
+        <?php
 // INI ADALAH KONEKSI KE DATABASE
 require_once("Koneksi.php");
 
@@ -9,15 +7,15 @@ $query = mysqli_query($koneksi,"SELECT * FROM buku");
 // print_r($query);
 
 // $data DIGUNAKAN UNTUK MENAMPUNG DATA DARI DATABASE DALAM BENTUK ARRAY
-$data = mysqli_fetch_array($query);
+// $data = mysqli_fetch_array($query);
 // $data2 = mysqli_fetch_row($query);
 // $data3 = mysqli_fetch_object($query);
 
-print_r($data);
+// print_r($data);
 // print_r($data2);
 // print_r($data3);
 
-echo "<br><br>";
+// echo "<br><br>";
 
 // CARA MENGGUNAKAN OBJECT
 // echo $data3 -> judul . "<br>";
@@ -27,12 +25,14 @@ echo "<br><br>";
 // print($data["1"]);
 // print($data["judul"]);
 
-echo "<br><br>";
+// echo "<br><br>";
 
 // print($data[""]);
 // echo "<center><img src = images/{$data["gambar"]} width=300></center>";
 
-
-
-
+// PERULANGAN KONEKSI
+// while($data = mysqli_fetch_object($query)){
+//     echo $data -> id  . "<br>";
+//     echo $data -> judul . "<br>";
+// };
 ?>
--- Perpustakaan/Tabelbuku.php
+++ Perpustakaan/Tabelbuku.php
@@ -1,5 +1,36 @@
-<?php
+<html>
+    <head>
+        <title>Tabel Buku</title>
+    </head>
+    <body>
+        <table border="" width="800">
+            <tr>
+                <th>No</th>
+                <th>Judul Buku</th>
+                <!-- <th>Gambar</th> -->
+                <th>Penulis</th>
+                <th>Penerbit</th>
+                <th>Cover</th>
+            </tr>
 
+            <?php
+            require_once("Koneksi.php");
+            $query = mysqli_query($koneksi,"SELECT * FROM buku");
+            $no = 1;
 
+            while ($data = mysqli_fetch_object($query)){
+                echo "
+                <tr>
+                <td>$no</td>
+                <td>{$data->judul}</td>
+                <td>{$data->penulis}</td>
+                <td>{$data->penerbit}</td>
+                <td>$no</td>
+                </tr>";
 
-?>
+                $no++;
+                }
+            ?>
+        </table>
+    </body>
+</html>
